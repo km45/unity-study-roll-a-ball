e263ee8945f137636d8eda43e5e8fb8f7e7f5615
Kamei Shogo <km45@users.noreply.github.com>
2019-10-27 04:03:34 +0900

    Complete step 6.
    https://github.com/unity3d-jp/FirstTutorial/wiki/6.%E3%82%B1%E3%82%99%E3%83%BC%E3%83%A0%E3%81%AE%E3%82%AF%E3%83%AA%E3%82%A2%E3%82%92%E8%BF%BD%E5%8A%A0%E3%81%99%E3%82%8B

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -3,10 +3,16 @@
 public class GameController : MonoBehaviour
 {
     public UnityEngine.UI.Text scoreLabel;
+    public GameObject winnerLabelObject;
 
     public void Update()
     {
         int count = GameObject.FindGameObjectsWithTag("Item").Length;
         scoreLabel.text = count.ToString();
+
+        if (count == 0)
+        {
+            winnerLabelObject.SetActive(true);
+        }
     }
 }
