1a84accdbd13c60b6ea8c9343287bfdffaa1d63d
Kamei Shogo <km45@users.noreply.github.com>
2019-10-27 19:03:19 +0900

    Complete step 7.
    https://github.com/unity3d-jp/FirstTutorial/wiki/7.%E9%9A%9C%E5%AE%B3%E7%89%A9%E3%81%A8%E3%83%AA%E3%82%B9%E3%82%BF%E3%83%BC%E3%83%88%E3%81%AE%E8%BF%BD%E5%8A%A0

---
+++ Assets/Script/DangerWall.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DangerWall : MonoBehaviour
+{
+    void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.CompareTag("Player"))
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
